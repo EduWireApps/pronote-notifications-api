2124508088450b5268e9c1a3ba77adbfe6a50211
Androz2091 <androz2091@gmail.com>
2020-11-15 19:04:45 +0100

    :bug: check if there is a note before showing it
    prevent "undefined/20" or something like that to be sent

--- services/pronote.js
+++ services/pronote.js
@@ -85,7 +85,7 @@ class PronoteService {
                                     notifications.push({
                                         type: 'mark',
                                         title: `Nouvelle note en ${markNotif.subject.name}`,
-                                        body: `Note: ${markNotif.mark.value}/${markNotif.mark.scale}\nMoyenne de la classe: ${markNotif.mark.average}/${markNotif.mark.scale}`
+                                        body: `${markNotif.mark.value ? `Note: ${markNotif.mark.value}/${markNotif.mark.scale}\n` : ''}Moyenne de la classe: ${markNotif.mark.average}/${markNotif.mark.scale}`
                                     })
                                 })
                             }
